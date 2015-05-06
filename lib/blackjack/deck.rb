067bf58d1e1821890cfa8f9849e1ace801f6e986
Ilya Konyukhov <runsfor@gmail.com>
2015-05-06 09:37:14 +0300

    Added ability to create a deck with provided cards

--- lib/blackjack/deck.rb
+++ lib/blackjack/deck.rb
@@ -1,14 +1,15 @@
 class Blackjack::Deck
   attr_reader :cards
 
-  def initialize
-    @cards = (0..51).map { |i| Blackjack::Card[i] }.shuffle
+  def initialize(*cards)
+    @cards = cards.empty? ? (0..51).map { |i| Blackjack::Card[i] }.shuffle : cards
   end
 
   def size
     cards.size
   end
 
+  # TODO: Throw Exception when there are no more cards in a deck
   def get(num = 0)
     cards.shift(num)
   end
